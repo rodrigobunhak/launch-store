cd761f19996952d5f041e729e3162f7e1c7fd89f
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2020-10-08 14:57:31 -0300

    Iniciando a estrutura de dados do upload de imagens

--- public/scripts.js
+++ public/scripts.js
@@ -16,3 +16,20 @@ const Mask = {
 
   }
 }
+
+const PhotosUpload = {
+  
+  uploadLimit: 6,
+
+  handleFileInput(event) {
+    
+    const { files: fileList } = event.target
+    const { uploadLimit } = PhotosUpload
+
+    if (fileList.length > uploadLimit) {
+      alert(`Envie no máximo ${uploadLimit} fotos`)
+      event.preventDefault()
+      return
+    }
+  }
+}
