3a1d14886f72a3af711b3adda383b80179b037b0
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2020-10-09 16:15:30 -0300

    Array de promessas com Promise.all()

--- src/routes.js
+++ src/routes.js
@@ -9,8 +9,9 @@ routes.get('/', function (req, res) {
 
 routes.get('/products/create', ProductController.create)
 routes.get('/products/:id/edit', ProductController.edit)
-routes.post('/products', ProductController.post)
-routes.put('/products', ProductController.put)
+
+routes.post('/products', multer.array("photos", 6), ProductController.post)
+routes.put('/products', multer.array("photos", 6), ProductController.put)
 routes.delete('/products', ProductController.delete)
 
 // alias
