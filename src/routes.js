6e40574382fd932611304307bb183a661866392e
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2020-11-03 13:50:21 -0300

    Estruturando página de pesquisa

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,15 @@ const routes = express.Router()
 const multer = require('./app/middlewares/muter')
 const ProductController = require("./app/controllers/ProductController")
 const HomeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/SearchController')
 
+// Home
 routes.get('/', HomeController.index)
 
+// Search
+routes.get('/products/search', SearchController.index)
+
+// Products
 routes.get('/products/create', ProductController.create)
 routes.get('/products/:id', ProductController.show)
 routes.get('/products/:id/edit', ProductController.edit)
@@ -18,5 +24,6 @@ routes.delete('/products', ProductController.delete)
 routes.get('/ads/create', function (req, res) {
   return res.redirect("/products/create")
 })
+
 
 module.exports = routes
